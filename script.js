8b6fac0b39be702b31add48b461d832a4eb2730e
AhmadGDev <Ahmad.ghelem@gmail.com>
2020-04-04 18:06:56 +0200

    calculate value

--- script.js
+++ script.js
@@ -3,4 +3,11 @@
 let button = "";
 
 function insert(btn) {
-    button = button + btn.value;
+  button = button + btn.value;
+
+  document.getElementById("showvalue").innerHTML = button;
+}
+
+function result() {
+  document.getElementById("showvalue").innerHTML = eval(button);
+}
